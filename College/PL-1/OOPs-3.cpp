bffd506a808732e339004fd8bea97222f8998ca2
Hemant Shashikant Yadav <hemantsryadav@gmail.com>
2022-11-08 14:34:40 +0530

    1

---
+++ College/PL-1/OOPs-3.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+class test
+{
+private:
+    int n;
+public:
+    test();
+    ~test();
+};
+
+test::test()
+{
+    n=10;
+    cout<<"Number = "<<n<<endl;
+}
+
+test::~test()
+{
+    cout<<"Object is destroyed.";
+}
+
+int main()
+{
+    test t;
+    return 0;
+}
