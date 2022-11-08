384d9e5033b25270ac3ed1b9f7b6a03b033b9085
Hemant Shashikant Yadav <hemantsryadav@gmail.com>
2022-11-08 15:40:21 +0530

    hh

--- College/PL-1/OOPs-4.cpp
+++ College/PL-1/OOPs-4.cpp
@@ -7,13 +7,14 @@ private:
     int opt;
     char name[50];
     long int ACCnumber;
-    float bal = 0, intr, amt, acc, rate, dep, time, wet,n,principal;
+    float bal = 0, intr, amt=0, acc, rate, dep, time, wet, n, principal;
 
 public:
     account();
     ~account();
     void deposite();
     void compund();
+    void simple();
     void menu();
     void withdraw();
     void displaybal();
@@ -53,26 +54,38 @@ void account::withdraw()
 void account::compund()
 {
     cout << "Enter principal ammount = ";
-    cin>>principal;
+    cin >> principal;
     cout << "Enter intrest rate = ";
-    cin>>rate;
+    cin >> rate;
     cout << "Enter time period = ";
-    cin>>time;
+    cin >> time;
     cout << "Enter number of times interest applied per time period = ";
-    cin>>n;
-    amt = principal*(pow((1 + rate / n), n*time));
+    cin >> n;
+    amt = principal * (pow((1 + ((rate/100) / n)),( n * time)));
 
-    cout<<"\vThe final amount is = "<<amt<<endl;
-    cout<<"\vThe compund intrest is = "<<amt-principal<<endl;
-
+    cout << "\vThe final amount is = " << amt << endl;
+    cout << "\vThe compund intrest is = " << amt - principal << endl;
+}
+void account::simple()
+{
+    cout << "Enter principal ammount = ";
+    cin >> principal;
+    cout << "Enter intrest rate = ";
+    cin >> rate;
+    
+    cout << "Enter time period = ";
+    cin >> time;
+    amt = principal * (1 + ((rate/100) * time));
+    cout << "\vThe final amount is = " << amt << endl;
+    cout << "\vThe simple intrest is = " << amt - principal << endl;
 }
 void account::menu()
 {
     while (1)
     {
-        cout << "\vWelcome \n1.Display balance\n2.Diposite\n3.Withdraw\n4.Compund intrest\n5.Exit\nEnter the option number = ";
+        cout << "\vWelcome \n1.Display balance\n2.Diposite\n3.Withdraw\n4.Compund intrest\n5.Simple intrest\n6.Exit\nEnter the option number = ";
         cin >> opt;
-        if (opt == 5)
+        if (opt == 6)
         {
             break;
         }
@@ -91,6 +104,9 @@ void account::menu()
                 break;
             case 4:
                 compund();
+                break;
+            case 5:
+                simple();
                 break;
 
             default:
