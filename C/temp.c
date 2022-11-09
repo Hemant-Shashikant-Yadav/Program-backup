989b51356b55abc76af2b075b4d2e34960f226d5
Hemant Shashikant Yadav <hemantsryadav@gmail.com>
2022-11-09 23:40:37 +0530

    h

--- C/temp.c
+++ C/temp.c
@@ -3,25 +3,7 @@
 int main()
 {
 
-    int i, dig, temp, cube, tot;
-
-    for (i = 1; i < 1000; i++)
-    {
-       temp=0;
-       tot=0;
-        for(temp=i;temp>0;temp/=10)
-        {
-            dig = temp % 10;
-            cube = dig * dig * dig;
-            tot += cube;
-            
-        }
-
-        if (tot==i)
-        {
-            printf("Armstron no = %d\n",i);
-        }
-    }
+    printf("%d",sizeof(char));
 
     return 0;
 }
