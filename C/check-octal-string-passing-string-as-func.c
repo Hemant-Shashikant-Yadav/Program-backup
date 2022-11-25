281ac48bdf65cdb73ef5327554a4519600dbbf28
Hemant Shashikant Yadav <hemantsryadav@gmail.com>
2022-11-25 20:57:42 +0530

    h

---
+++ C/check-octal-string-passing-string-as-func.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main()
+{
+
+    char p[50];
+    int checkOctal(char[]);
+
+    printf("Enter string = ");
+    scanf("%s", &p);
+
+    if (checkOctal(p) == 0)
+    {
+        printf("Not octal");
+    }
+    else
+    {
+        printf("Octal");
+    }
+
+    return 0;
+}
+int checkOctal(char a[])
+{
+    int i = 0;
+    while (a[i] != '\0')
+    {
+        if (a[i] >= '0' && a[i] <= '8')
+        {
+            i++;
+        }
+        else
+        {
+            break;
+        }
+    }
+    if (a[i] == '\0')
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
