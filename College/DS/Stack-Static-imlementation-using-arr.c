f5514324bec8bb2fbbc3eb0830656eb3b02c8a08
Hemant Shashikant Yadav <101739773+Hemant-Shashikant-Yadav@users.noreply.github.com>
2022-12-08 11:15:36 +0530

    Update Stack-Static-imlementation-using-arr.c

--- College/DS/Stack-Static-imlementation-using-arr.c
+++ College/DS/Stack-Static-imlementation-using-arr.c
@@ -6,6 +6,13 @@ void Traverse();
 void TopOfStack();
 int main()
 {
+
+    int ch;
+    while (1)
+    {
+        printf("HII\n1.Push\n2.Pop\n3.Top of stack\n4.Traverse\n5.Exit")
+    }
+    
     
  
     return 0;
