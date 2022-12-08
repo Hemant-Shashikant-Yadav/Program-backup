e670e45549b8bd10ba6a1f2e5b5fa2e669f8300d
Hemant Shashikant Yadav <hemantsryadav@gmail.com>
2022-12-08 21:29:09 +0530

    kk

--- College/DS/Stack-Static-imlementation-using-arr.c
+++ College/DS/Stack-Static-imlementation-using-arr.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 
 void PUSH();
 void POP();
@@ -10,10 +10,18 @@ int main()
     int ch;
     while (1)
     {
-        printf("HII\n1.Push\n2.Pop\n3.Top of stack\n4.Traverse\n5.Exit")
+        printf("HII\n1.Push\n2.Pop\n3.Top of stack\n4.Traverse\n5.Exit");
+        scanf("%d", &ch);
+        switch (ch)
+        {
+        case 1:
+            PUSH();
+            break;
+
+        default:
+            break;
+        }
     }
-    
-    
- 
+
     return 0;
 }
