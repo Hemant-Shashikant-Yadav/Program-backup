bcfcb62e7f0daa3b8d5adc1864169fe6492c4a14
Hemant Shashikant Yadav <hemantsryadav@gmail.com>
2022-12-11 21:46:09 +0530

    O

---
+++ C++/OOPs_25_class_arrow-operator-pointer-object.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+class complex
+{
+private:
+    int real, imag;
+
+public:
+    void set_data();
+    void get_data();
+};
+void complex::set_data()
+{
+    cout << "Enter the real part = ";
+    cin >> real;
+    cout << "Enter the imaginary part = ";
+    cin >> imag;
+}
+void complex::get_data()
+{
+    cout << "The real part = " << real << endl;
+    cout << "The imaginary part = " << imag << endl;
+}
+
+int main()
+{
+    // complex C;
+    // complex *ptr = &C;
+    // (ptr)->set_data();
+    // (ptr)->get_data();
+
+    complex *ptr = new complex;
+    ptr->set_data();
+    ptr->get_data();
+    return 0;
+}
