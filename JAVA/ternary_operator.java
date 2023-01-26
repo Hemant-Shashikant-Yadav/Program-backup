3d99c033f407b062e7a07f31b04c69d15ce6873d
Hemant Shashikant Yadav <hemantsryadav@gmail.com>
2023-01-26 15:55:20 +0530

    jj

--- JAVA/ternary_operator.java
+++ JAVA/ternary_operator.java
@@ -1,14 +1,14 @@
-public class ternary_operator {
+// public class ternary_operator {
 
-    public static void main(String[] args) {
-        int a;
-        String s;
+//     public static void main(String[] args) {
+//         int a;
+//         String s;
 
-        a = (10 > 30) ? 10 : 30;
-        // b= (20>10)? 1:0;
-        s = (10 % 2 == 0) ? "Even" : "Odd";
+//         a = (10 > 30) ? 10 : 30;
+//         // b= (20>10)? 1:0;
+//         s = (10 % 2 == 0) ? "Even" : "Odd";
 
-        System.out.println(+a);
-        System.out.println(s);
-    }
-}
+//         System.out.println(+a);
+//         System.out.println(s);
+//     }
+// }
