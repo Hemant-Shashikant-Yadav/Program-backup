2da1a8404e3ac40c114b50f1c54dc463d5459b4a
Hemant Shashikant Yadav <hemantsryadav@gmail.com>
2023-06-12 08:03:46 +0530

    aa

--- College/JAVA/EXPT 6/Q4LibraryApp.java
+++ College/JAVA/EXPT 6/Q4LibraryApp.java
@@ -1,4 +1,5 @@
-import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 interface LibraryItem {
@@ -25,18 +26,80 @@ class Book implements LibraryItem {
 
     }
 
- 
+    List<bk> b1 = new LinkedList<bk>();
+    bk b[] = new bk[100];
+
+    void init() {
+        for (int i = 0; i < b.length; i++) {
+            b[i] = new bk();
+            b[i].ISBN = 0;
+            b[i].author = "";
+            b[i].title = "";
+            b[i].availability = false;
+        }
+    }
+
+    public void add() {
+        bkcount++;
+        System.out.print("Enter name of book =");
+        b[bkcount].title = sc.nextLine();
+        System.out.print("Enter author name of book =");
+        b[bkcount].author = sc.nextLine();
+        System.out.print("Enter ISBN of book =");
+        b[bkcount].ISBN = sc.nextInt();
+        b[bkcount].availability = true;
+        // b1.add(b[bkcount]);
+    }
 
     public void checkOut() {
+        System.out.println("Enter ISBN number = ");
+        int no = sc.nextInt();
+        int i = 0;
+        while (i <= bkcount) {
+            if (no == b[i].ISBN) {
+
+                if (b[no].availability == true) {
+                    System.out.println("Book is available and issued.");
+                    b[no].availability = false;
+                } else {
+                    System.out.println("Book not available .");
+
+                }
+            }
+            i++;
+        }
 
     }
 
     public void checkIn() {
 
+        System.out.println("Enter ISBN number = ");
+        int no = sc.nextInt();
+        int i = 0;
+        while (i <= bkcount) {
+            if (no == b[i].ISBN) {
+
+                System.out.println("Book is deposited.");
+                b[no].availability = true;
+            }
+            i++;
+        }
     }
 
     public boolean isAvailable() {
-        return true;
+        System.out.println("Enter ISBN number = ");
+        int no = sc.nextInt();
+        int i = 0;
+        while (i <= bkcount) {
+            if (no == b[i].ISBN) {
+
+                if (b[no].availability == true) {
+                    return true;
+                }
+            }
+            i++;
+        }
+        return false;
     }
 
 }
@@ -44,6 +107,7 @@ class Book implements LibraryItem {
 class DVD implements LibraryItem {
     static int cdcount = 0;
     Scanner sc = new Scanner(System.in);
+    List<cd> d1 = new LinkedList<cd>();
 
     class cd {
         String title, author, duration;
@@ -54,6 +118,16 @@ class DVD implements LibraryItem {
 
     cd d[] = new cd[100];
 
+    void init() {
+        for (int i = 0; i < d.length; i++) {
+            d[i] = new cd();
+            d[i].ISBN = 0;
+            d[i].author = "";
+            d[i].title = "";
+            d[i].availability = false;
+        }
+    }
+
     public void add() {
         cdcount++;
         System.out.print("Enter name of book =");
@@ -62,24 +136,64 @@ class DVD implements LibraryItem {
         d[cdcount].author = sc.nextLine();
         System.out.print("Enter ISBN of book =");
         d[cdcount].ISBN = sc.nextInt();
-
+        d1.add(d[cdcount]);
     }
 
     public void checkOut() {
+        System.out.println("Enter ISBN number = ");
+        int no = sc.nextInt();
+        int i = 0;
+        while (i <= cdcount) {
+            if (no == d[i].ISBN) {
 
+                if (d[no].availability == true) {
+                    System.out.println("CD is available and issued.");
+                    d[no].availability = false;
+                } else {
+                    System.out.println("CD not available .");
+
+                }
+            }
+            i++;
+        }
     }
 
     public void checkIn() {
 
+        System.out.println("Enter ISBN number = ");
+        int no = sc.nextInt();
+        int i = 0;
+        while (i <= cdcount) {
+            if (no == d[i].ISBN) {
+
+                System.out.println("CD is deposited.");
+                d[no].availability = true;
+            }
+            i++;
+        }
     }
 
     public boolean isAvailable() {
-        return true;
+        System.out.println("Enter ISBN number = ");
+        int no = sc.nextInt();
+        int i = 0;
+        while (i <= cdcount) {
+            if (no == d[i].ISBN) {
+
+                if (d[no].availability == true) {
+                    return true;
+                }
+            }
+            i++;
+        }
+        return false;
     }
 }
 
 class Library implements LibraryItem {
-        
+    public void add() {
+
+    }
 
     public void checkOut() {
 
@@ -95,7 +209,16 @@ class Library implements LibraryItem {
 }
 
 public class Q4LibraryApp {
-    Book B = new Book();
-    DVD D = new DVD();
-    Library L = new Library();
+    public static void main(String[] args) {
+        Book B = new Book();
+        DVD D = new DVD();
+        Library L = new Library();
+        B.init();
+        B.add();
+        B.checkOut();
+        System.out.println(B.isAvailable());
+        B.checkOut();
+        B.checkIn();
+        System.out.println(B.isAvailable());
+    }
 }
