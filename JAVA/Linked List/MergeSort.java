76e76f20f6616fa1f4ec5a3048210f536a8aac2b
hemantLinux <hemantsryadav@gmail.com>
2023-06-14 22:28:13 +0530

    aa

--- JAVA/Linked List/MergeSort.java
+++ JAVA/Linked List/MergeSort.java
@@ -17,6 +17,44 @@ public class MergeSort {
     public static Node head;
     public static Node tail;
 
+    void addEnd(int data) {
+        Node llNode = new Node(data);
+        if (head == null) {
+            head.next = tail.next = llNode;
+            return;
+        }
+
+        tail.next = llNode;
+        tail = llNode;
+
+    }
+
+    public Node findMid(Node head) {
+        Node slow = head, fast = head;
+        while (fast != null || fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public Node mergeSort(Node head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        Node mid = findMid(head);
+
+        Node newNode = mid;
+        mid.next = null;
+
+        mergeSort(head);
+        mergeSort(newNode);
+
+        merge(head, newNode);
+    }
+
+    public void merge(Node )
+
     public static void main(String[] args) {
 
     }
