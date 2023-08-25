fd15cb16cef783c0a9aa4aa2041a448bd4b3c2b2
Hemant Shashikant Yadav. <hemantsryadav@gmail.com>
2023-08-25 15:23:50 +0530

    25 aug c1

---
+++ Python/Cource/5GlobalVariableKeyword.py
@@ -0,0 +1,22 @@
+
+# x = "awesome"
+#
+# def myfunc():
+#   x = "fantastic"
+#   print("Python is " + x)
+#
+# myfunc()
+#
+# print("Python is " + x)
+#
+
+
+x = "awesome"
+
+def myfunc():
+  global x
+  x = "fantastic"
+
+myfunc()
+
+print("Python is " + x)
