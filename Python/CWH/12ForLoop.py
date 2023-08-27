6d0103920e38b9d9cc64091d6e729cb9b3328e45
Hemant Shashikant Yadav. <hemantsryadav@gmail.com>
2023-08-27 14:49:52 +0530

    27 aug c1

--- Python/CWH/12ForLoop.py
+++ Python/CWH/12ForLoop.py
@@ -1,7 +1,30 @@
 # Traverse each letter in string
 
-name = "Yadav"
-for i in name:
-    print(i)
-    if i == "a":
-        print("A")
+# name = "Yadav"
+# for i in name:
+#     print(i)
+#     if i == "a":
+#         print("A")
+
+# #Traversing the array
+# colors = ["Red", "Green", "Blue", "Yellow"]
+# for color in colors:
+#     print(color)
+#
+# #Traversing each letter in array saperately
+# colors = ["Red", "Green", "Blue", "Yellow"]
+# for color in colors:
+#     for letter in color:
+#         print(letter)
+
+# #Print number in given range
+# for k in range(5):
+#     print(k )
+#
+# #Print number in given range
+# for I in range(1,15):
+#     print(I)
+#
+# #print number in given range by specified jump
+# for j in range(1,20,2):
+#     print(j)
---
+++ Python/CWH/13WhileDoWhile.py
@@ -0,0 +1,17 @@
+
+#While loop
+i=0;
+while(i<10):
+    print(i)
+    i+=1
+
+print(i)
+
+#Do while loop
+while True:
+    print(i)
+    i-=1
+    if i>=0:
+        continue
+    else:
+        break
