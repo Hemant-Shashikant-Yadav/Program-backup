bb6e730c067bac9040eb529557a4c2eaa48d7dd9
Hemant Shashikant Yadav. <hemantsryadav@gmail.com>
2023-08-29 22:41:03 +0530

    hh

--- College/JAVA/Expt 10/Test1/package_name/ClassOne.java
+++
@@ -1,7 +0,0 @@
-package package_name;
-
-public class ClassOne {
-	public void methodClassOne() {
-		System.out.println("Hello there its ClassOne");
-	}
-}
--- College/JAVA/Expt 10/Test1/package_one/ClassTwo.java
+++
@@ -1,8 +0,0 @@
-
-package package_one;
-  
-public class ClassTwo {
-    public void methodClassTwo(){
-        System.out.println("Hello there i am ClassTwo");
-    }    
-}
